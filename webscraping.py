76bbb924c9aea28a29470020661dc00690338da7
Stanlin Joseph <smjoseph11@gmail.com>
2019-07-14 17:41:37 -0500

    changed some data structures to stuff that makes more sense

--- webscraping.py
+++ webscraping.py
@@ -1,3 +1,4 @@
+import atexit
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
@@ -8,17 +9,45 @@ start_page_url='https://www.1800contacts.com/lenses'
 Expected_DOM_elem='ProductLink'
 def prompt_user():
     xpaths=[]
-    user_continues=True
-    while user_continues:
+    another_xpath=True
+    while another_xpath:
         xpath_text = raw_input("Please provide an xpath you want to scrape information from in the form(relative: //tagname[@attribute=value] or absolute: '/html/.../' ): ")
-        elemortext= raw_input("Please provide the element name you want the text from or type \"text\" for the elements inner text: ")
-        xpaths.append((xpath_text, elemortext))
-        while(True):
+        dict_of_xpath_elems={}
+        list_of_elems=[]
+        another_elem=True
+        while another_elem:
+            elemortext= raw_input("Please provide the element name you want the text from or type \"text\" for the elements inner text: ")
+            list_of_elems.append(elemortext)
+            while True:
+                another=raw_input("Would you like to add another element(Y,n)?")
+                try:
+                    if str(another)[0].lower()=='n':
+                        print('if')
+                        another_elem=False
+                        break
+                    elif str(another)[0].capitalize()=='Y':
+                        print('elif')
+                        break
+                    else:
+                        print('else')
+                        continue
+                except(IndexError):
+                    continue
+        dict_of_xpath_elems[xpath_text]=list_of_elems
+        xpaths.append(dict_of_xpath_elems)
+        while True:
             another=raw_input("Would you like to add another xpath(Y,n)?")
             try:
-                if str(another)[0].capitalize()!="Y":
-                    user_continues=False;
-                break
+                if str(another)[0].lower()=='n':
+                    print('if')
+                    another_xpath=False
+                    break
+                elif str(another)[0].capitalize()=='Y':
+                    print('elif')
+                    break
+                else:
+                    print('else')
+                    continue
             except(IndexError):
                 continue
     return xpaths;
@@ -41,25 +70,30 @@ for product in products:
     product_urls.append(product.get_attribute('href'))
 first=True;
 #TODO: dict_of_results structured like so {"url": "", "xpath": "", "info":""} I'm going to want to be able to get multiple attributes per xpath pass instead of repeating the same xpath for a different element
-dict_of_results={} 
+list_of_results=[] 
 for product_url in product_urls:
-		dict_of_results["url"]=product_url	
+    dict_of_url_info={}
+    dict_of_url_info['url']=product_url
+    list_of_results.append(dict_of_url_info)	
     url=driver.get(product_url)
-    for xpath in xpaths:
-      expected_xpath=xpath[0]
-      expected_element=xpath[1]
-      try:
-          WebDriverWait(driver,10).until(
-          EC.presence_of_element_located((By.XPATH,expected_xpath))
-          )
-      except TimeoutException:
-          print("could not find xpath " + expected_xpath+"! At URL="+str(product_url)+". Continuing...")
-      else:
-				dict_of_results["xpath"]=expected_xpath
-        dom_xpath_obj = driver.find_element_by_xpath(expected_xpath)
-        if expected_element=="text":
-					dict_of_results["info"]=expected_xpath
-          info=dom_xpath_obj.text
-        else:
-          info=dom_xpath_obj.get_attribute(expected_element)
+    for dict_of_xpath_and_elems in xpaths:
+        for xpath, elems in dict_of_xpath_and_elems.items():
+          expected_xpath=xpath.lower()
+          try:
+              WebDriverWait(driver,10).until(
+              EC.presence_of_element_located((By.XPATH,expected_xpath))
+              )
+          except TimeoutException:
+              print("could not find xpath " + expected_xpath+"! At URL="+str(product_url)+". Continuing...")
+          else:
+            dict_of_url_info["xpath"]=expected_xpath
+            dom_xpath_obj = driver.find_element_by_xpath(expected_xpath)
+            for expected_element in elems:
+                if expected_element=="text":
+                    info=dom_xpath_obj.text
+                    dict_of_url_info["info"]=info
+                else:
+                    info=dom_xpath_obj.get_attribute(expected_element)
+                    dict_of_url_info["info"]=info
+    print(list_of_results)
 
